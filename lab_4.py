6c4cf4c847a63d5dc619981c7473a620e5a05172
arsen-zaharenko <59254411+arsen-zaharenko@users.noreply.github.com>
2022-04-03 15:29:28 +0300

    add lab 4

---
+++ lab_4.py
@@ -0,0 +1,64 @@
+from random import randint
+
+def print_matrix(matrix: list):
+	for row in matrix:
+		print(' '.join(map(str,row)))
+	print()
+
+def create_matrix(N: int, S: int) -> list:
+	matrix = [[randint(1, 3) for i in range(S)] for j in range(N)]
+
+	return matrix
+
+def divide_matrix(matrix: list, P: int) -> list:
+	row_size = len(matrix[0])
+	matrices_number = len(matrix) // P
+
+	while matrices_number * P - len(matrix):
+		matrix.append([0] * row_size)
+		matrices_number = len(matrix) // P
+
+	matrices = [[matrix[matrices_number * i + j] for j in range(matrices_number)] for i in range(P)]
+
+	return matrices
+
+def create_multimatrix(matrices: list, P: int) -> list:
+	row_size = len(matrices[0][0])
+	column_size = len(matrices[0])
+
+	zero_matrix = [[0] * row_size for i in range(column_size)]
+
+	multimatrix = [matrices[i:] + [zero_matrix] * i if i else matrices for i in range(len(matrices))]
+
+	multimatrix_rows = [[] for i in range(column_size * len(multimatrix))] 
+
+	for i, matrices in enumerate(multimatrix):
+		for j, matrix in enumerate(matrices):
+			for k, row in enumerate(matrix):
+				multimatrix_rows[i * column_size + k].append(row)
+
+	multimatrix = [[] for i in range(column_size * len(multimatrix))]
+	for i, row in enumerate(multimatrix_rows):
+		for subrow in row:
+			multimatrix[i] += subrow
+
+	return multimatrix
+
+
+def main():
+	N = 9
+	S = 4
+	P = 3
+
+	matrix = create_matrix(N, S)
+	print_matrix(matrix)
+
+	matrices = divide_matrix(matrix, P)
+	for submatrix in matrices:
+		print_matrix(submatrix)
+
+	multimatrix = create_multimatrix(matrices, P)
+	print_matrix(multimatrix)
+
+if __name__ == '__main__':
+	main()
